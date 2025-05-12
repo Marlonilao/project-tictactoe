3e137d047abacbaceb7cde0124695b07ce9b62e1
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-05-12 15:39:22 +0800

    wrap up features, just need to finish up the CSS styling

--- script.js
+++ script.js
@@ -1,99 +1,3 @@
-// const tictactoe = (function (){
-//     let gameBoard = {row1:[" "," "," "],
-//                      row2:[" "," "," "],
-//                      row3:[" "," "," "]};
-                    
-//     function displayBoard() {
-//         console.log(`${gameBoard.row1}\n${gameBoard.row2}\n${gameBoard.row3}`)
-//     }
-
-//     const players = {
-//         x: function(area) {
-//             const rowIndex = Math.floor((area - 1) / 3);
-//             const colIndex = (area - 1) % 3;
-//             const rowKey = `row${rowIndex + 1}`;
-//             if (gameBoard[rowKey][colIndex] !== " ") {
-//                 console.log("Error: that area is already marked");
-//             } else {
-//             gameBoard[rowKey][colIndex] = 'X';
-//             }
-//         },
-//         o: function(area) {
-//             const rowIndex = Math.floor((area - 1) / 3);
-//             const colIndex = (area - 1) % 3;
-//             const rowKey = `row${rowIndex + 1}`;
-//             if (gameBoard[rowKey][colIndex] !== " ") {
-//                 console.log("Error: that area is already marked");
-//             } else {
-//             gameBoard[rowKey][colIndex] = 'O';
-//             }
-//         }
-//     }
-
-//     function checkBoardforWinner(){ 
-//         if (gameBoard.row1[0] == "X" && gameBoard.row1[1] == "X" && gameBoard.row1[2] == "X"){
-//             console.log('player X wins')
-//         } else if (gameBoard.row2[0] == "X" && gameBoard.row2[1] == "X" && gameBoard.row2[2] == "X") {
-//             console.log('Player X wins');
-//         } else if (gameBoard.row3[0] == "X" && gameBoard.row3[1] == "X" && gameBoard.row3[2] == "X") {
-//             console.log('Player X wins');
-//         } else if (gameBoard.row1[0] == "O" && gameBoard.row1[1] == "O" && gameBoard.row1[2] == "O"){
-//             console.log('player O wins');
-//         } else if (gameBoard.row2[0] == "O" && gameBoard.row2[1] == "O" && gameBoard.row2[2] == "O") {
-//             console.log('Player O wins');
-//         } else if (gameBoard.row3[0] == "O" && gameBoard.row3[1] == "O" && gameBoard.row3[2] == "O") {
-//             console.log('Player O wins');
-//         } else if (gameBoard.row1[0] == "X" && gameBoard.row2[0] == "X" && gameBoard.row3[0] == "X") {
-//             console.log('Player X wins');
-//         } else if (gameBoard.row1[1] == "X" && gameBoard.row2[1] == "X" && gameBoard.row3[1] == "X") {
-//             console.log('Player X wins');
-//         } else if (gameBoard.row1[2] == "X" && gameBoard.row2[2] == "X" && gameBoard.row3[2] == "X") {
-//             console.log('Player X wins');
-//         } else if (gameBoard.row1[0] == "O" && gameBoard.row2[0] == "O" && gameBoard.row3[0] == "O") {
-//             console.log('Player O wins');
-//         } else if (gameBoard.row1[1] == "O" && gameBoard.row2[1] == "O" && gameBoard.row3[1] == "O") {
-//             console.log('Player O wins');
-//         } else if (gameBoard.row1[2] == "O" && gameBoard.row2[2] == "O" && gameBoard.row3[2] == "O") {
-//             console.log('Player O wins');
-//         } else if (gameBoard.row1[0] == "X" && gameBoard.row2[1] == "X" && gameBoard.row3[2] == "X") {
-//             console.log('Player X wins');
-//         } else if (gameBoard.row1[2] == "X" && gameBoard.row2[1] == "X" && gameBoard.row3[0] == "X") {
-//             console.log('Player X wins');
-//         } else if (gameBoard.row1[0] == "O" && gameBoard.row2[1] == "O" && gameBoard.row3[2] == "O") {
-//             console.log('Player O wins');
-//         } else if (gameBoard.row1[2] == "O" && gameBoard.row2[1] == "O" && gameBoard.row3[0] == "O") {
-//             console.log('Player O wins');
-//         } else if (!gameBoard.row1.includes(" ") && !gameBoard.row2.includes(" ") && !gameBoard.row3.includes(" ")) {
-//             console.log("Its a tie!")
-//         }
-//     }
-
-//     function gameFlow() {
-//         const playerXname = "Player X";
-//         const playerOname = "Player O";
-
-//         const players = [
-//             {name: playerXname,
-//              mark: "X"
-//             },
-//             {name: playerOname,
-//              mark: "O"
-//             }
-//         ]
-
-//         let activePlayer = players[0];
-
-//         const switchPlayerTurn = () => {
-//             activePlayer = activePlayer == players[0] ? players[1] : players[0];
-//         }
-
-//         return {switchPlayerTurn}
-//     }
-    
-//     return {gameBoard, players, checkBoardforWinner, displayBoard}
-
-// })();
-
 const tictactoe = (function() {
     let gameBoard = {row1:[" "," "," "],
                      row2:[" "," "," "],
@@ -104,58 +8,89 @@ const tictactoe = (function() {
         console.log(`${gameBoard.row1}\n${gameBoard.row2}\n${gameBoard.row3}`)
     }
 
+    function clearBoard() {
+        for (let i=0; i<3; i++) {
+            gameBoard[`row${i+1}`][0] = " "
+            gameBoard[`row${i+1}`][1] = " "
+            gameBoard[`row${i+1}`][2] = " "
+        }
+    }
+
+    function clearDOMBoard() {
+        for (i=0; i<9; i++) {
+            displayDOMLogic.tictactoeBox[i].textContent = " ";
+        }
+    }
+
     function checkBoardforWinner(){ 
         if (gameBoard.row1[0] == "X" && gameBoard.row1[1] == "X" && gameBoard.row1[2] == "X"){
-            console.log('player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row2[0] == "X" && gameBoard.row2[1] == "X" && gameBoard.row2[2] == "X") {
-            console.log('Player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row3[0] == "X" && gameBoard.row3[1] == "X" && gameBoard.row3[2] == "X") {
-            console.log('Player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[0] == "O" && gameBoard.row1[1] == "O" && gameBoard.row1[2] == "O"){
-            console.log('player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row2[0] == "O" && gameBoard.row2[1] == "O" && gameBoard.row2[2] == "O") {
-            console.log('Player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row3[0] == "O" && gameBoard.row3[1] == "O" && gameBoard.row3[2] == "O") {
-            console.log('Player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[0] == "X" && gameBoard.row2[0] == "X" && gameBoard.row3[0] == "X") {
-            console.log('Player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[1] == "X" && gameBoard.row2[1] == "X" && gameBoard.row3[1] == "X") {
-            console.log('Player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[2] == "X" && gameBoard.row2[2] == "X" && gameBoard.row3[2] == "X") {
-            console.log('Player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[0] == "O" && gameBoard.row2[0] == "O" && gameBoard.row3[0] == "O") {
-            console.log('Player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[1] == "O" && gameBoard.row2[1] == "O" && gameBoard.row3[1] == "O") {
-            console.log('Player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[2] == "O" && gameBoard.row2[2] == "O" && gameBoard.row3[2] == "O") {
-            console.log('Player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[0] == "X" && gameBoard.row2[1] == "X" && gameBoard.row3[2] == "X") {
-            console.log('Player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[2] == "X" && gameBoard.row2[1] == "X" && gameBoard.row3[0] == "X") {
-            console.log('Player X wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player X wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[0] == "O" && gameBoard.row2[1] == "O" && gameBoard.row3[2] == "O") {
-            console.log('Player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (gameBoard.row1[2] == "O" && gameBoard.row2[1] == "O" && gameBoard.row3[0] == "O") {
-            console.log('Player O wins');
+            displayDOMLogic.getFooterMessage().textContent = 'Player O wins';
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else if (!gameBoard.row1.includes(" ") && !gameBoard.row2.includes(" ") && !gameBoard.row3.includes(" ")) {
-            console.log("Its a tie!")
+            displayDOMLogic.getFooterMessage().textContent = ("Its a tie!")
             gameControl.setGameStatus(false);
+            gameControl.disableGame();
         } else {
             return;
         }
@@ -165,41 +100,12 @@ const tictactoe = (function() {
         return gameBoard[row];
     }
 
-    return {getBoard, displayBoard, checkBoardforWinner}
+    return {getBoard, displayBoard, checkBoardforWinner, clearBoard, clearDOMBoard}
 
 })();
 
 const players = (function() {
 
-    function x(area) {
-        const rowIndex = Math.floor((area - 1) / 3);
-        const colIndex = (area - 1) % 3;
-        const rowKey = `row${rowIndex + 1}`;
-        if (tictactoe.getBoard(rowKey)[colIndex] !== " ") {
-            console.log("Error: that area is already marked");
-        } else {
-        tictactoe.getBoard(rowKey)[colIndex] = 'X';
-        }
-    }
-    function o(area) {
-        const rowIndex = Math.floor((area - 1) / 3);
-        const colIndex = (area - 1) % 3;
-        const rowKey = `row${rowIndex + 1}`;
-        if (tictactoe.getBoard(rowKey)[colIndex] !== " ") {
-            console.log("Error: that area is already marked");
-        } else {
-        tictactoe.getBoard(rowKey)[colIndex] = 'O';
-        }
-    }
-
-
-    return {x,o}
-})();
-
-
-const gameControl = (function(){
-
-    
     const playerX = 'Player X';
     const playerO = 'Player O';
 
@@ -207,43 +113,133 @@ const gameControl = (function(){
 
     let activePlayer = playersArray[0];
 
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer == playersArray[0] ? playersArray[1] : playersArray [0];
+        displayDOMLogic.getFooterMessage().textContent = `${activePlayer}'s turn`;
+    }
+
+    function x(area) {
+        const rowIndex = Math.floor((area - 1) / 3);
+        const colIndex = (area - 1) % 3;
+        const rowKey = `row${rowIndex + 1}`;
+
+        try {
+            if (tictactoe.getBoard(rowKey)[colIndex] !== " ") {
+                throw "Error: that area is already marked, still Player X's turn"
+            }
+            tictactoe.getBoard(rowKey)[colIndex] = 'X';
+            displayDOMLogic.tictactoeBox[area-1].textContent = 'X';
+            if (gameControl.getGameStatus()) {
+                        switchPlayerTurn();
+                    }
+        }
+        catch (err) {
+            displayDOMLogic.getFooterMessage().textContent = err;
+        }
+    }
+
+    function o(area) {
+        const rowIndex = Math.floor((area - 1) / 3);
+        const colIndex = (area - 1) % 3;
+        const rowKey = `row${rowIndex + 1}`;
+
+        try {
+            if (tictactoe.getBoard(rowKey)[colIndex] !== " ") {
+                throw "Error: that area is already marked, still Player O's turn"
+            }
+            tictactoe.getBoard(rowKey)[colIndex] = 'O';
+            displayDOMLogic.tictactoeBox[area-1].textContent = 'O';
+            if (gameControl.getGameStatus()) {
+                        switchPlayerTurn();
+                    }
+        }
+        catch (err) {
+            displayDOMLogic.getFooterMessage().textContent = err;
+        }
+    }
+
+    function clickHandler(e) {
+        const box = e.currentTarget;
+        if (activePlayer == playersArray[0]) {
+            x(+box.id);
+            tictactoe.checkBoardforWinner();
+        } else {
+            o(+box.id);
+            tictactoe.checkBoardforWinner();
+        }
+    }
+
+    function getPlayerMove() {
+        displayDOMLogic.tictactoeBox.forEach(box => {
+            box.addEventListener('click' , clickHandler)
+        })
+    }
+
+    return {x, o, getPlayerMove, getActivePlayer, clickHandler}
+})();
+
+
+const gameControl = (function(){
+
+    
     let gameStatus = false;
 
     function setGameStatus(status) {
         gameStatus = status;
     }
 
-    const getActivePlayer = () => activePlayer;
-
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer == playersArray[0] ? playersArray[1] : playersArray [0];
-    }
-    // reminders: transfer this function to each players function.
-
-
-    function getPlayerMove() {
-        let playerMove = prompt('')
-        if (activePlayer == playersArray[0]) {
-            players.x(+playerMove);
-        } else {
-            players.o(+playerMove);
-        }
+    function getGameStatus() {
+        return gameStatus;
     }
 
-    function gameFlow(){
+    function gameStart(){
         gameStatus = true;
-        while (gameStatus) {
-            tictactoe.displayBoard();
-            console.log(`${activePlayer}'s turn`)
-            getPlayerMove();
-            tictactoe.checkBoardforWinner();
-            console.log(`gameStatus is ${gameStatus}`)
-            if (gameStatus) {
-                switchPlayerTurn();
-            }
-        };
+        players.getPlayerMove();
     }
 
-    return {gameFlow, getActivePlayer, setGameStatus}
+    function disableGame() {
+        if (gameStatus == false) {
+            displayDOMLogic.tictactoeBox.forEach(box => {
+                box.removeEventListener('click' , players.clickHandler)
+            })
+        }
+        
+    }
 
+    return {gameStart, setGameStatus, getGameStatus, disableGame}
+
+})();
+
+const displayDOMLogic = (function() {
+    // const header = document.querySelector('.header');
+    // const headerMessage = document.querySelector('.header > p');
+    const tictactoeContainer = document.querySelector('.main');
+    const tictactoeBox = document.querySelectorAll('.main > div');
+    // const footer = document.querySelector('.footer');
+    const footerMessage = document.querySelector('.footer > p');
+    const gameStartButton = document.querySelector('.header > p > #start');
+    const restartButton = document.querySelector('.header >  p > #restart');
+
+    const tictactoeArray = Array.from(tictactoeBox)
+
+    function getFooterMessage() {
+        return footerMessage;
+    }
+
+    gameStartButton.addEventListener('click', e => {
+        gameControl.gameStart();
+        footerMessage.textContent = `${players.getActivePlayer()} move first`;
+    })
+    
+    restartButton.addEventListener('click' , e => {
+        tictactoe.clearBoard();
+        tictactoe.clearDOMBoard();
+        gameControl.gameStart();
+        footerMessage.textContent = `${players.getActivePlayer()} move first`;
+    })
+
+
+    return {tictactoeBox, footerMessage, tictactoeArray, tictactoeContainer, getFooterMessage, gameStartButton, tictactoeContainer}
 })();
